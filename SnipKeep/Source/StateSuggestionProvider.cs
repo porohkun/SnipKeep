065635f4909f648ccc0c6a353efacf7c79eec937
poroh-kun <meta.poroh.kun@gmail.com>
2020-04-27 18:26:13 +0300

    reorganize dependencies

--- SnipKeep/Source/StateSuggestionProvider.cs
+++ SnipKeep/Source/StateSuggestionProvider.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using AutoCompleteTextBox.Editors;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using WpfControls.Editors;
 
 namespace SnipKeep
 {
